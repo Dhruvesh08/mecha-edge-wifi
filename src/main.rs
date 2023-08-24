819e77ea9e551e97fe79f3fa6105fb89419ea250
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-08-24 18:39:30 +0530

    list of known wifi network

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 // main.rs (in your project)
 mod wifi_lib;
-use wifi_lib::get_wifi_list;
+use wifi_lib::get_known_wifi_list;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let wifi_list = get_wifi_list().await?;
+    let wifi_list = get_known_wifi_list().await?;
     println!("Wi-Fi networks: {:?}", wifi_list);
     Ok(())
 }
--- src/wifi_lib.rs
+++ src/wifi_lib.rs
@@ -2,7 +2,7 @@
 
 use env_logger::Env;
 use log::{error, info};
-use wifi_ctrl::{sta::{self, ScanResult}, Result};
+use wifi_ctrl::{sta::{self, ScanResult, NetworkResult}, Result};
 
 pub async fn get_wifi_list() -> Result<Vec<ScanResult>> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
@@ -39,6 +39,45 @@ async fn app(requester: sta::RequestClient) -> Result<Vec<ScanResult>> {
     requester.shutdown().await?;
     Ok(scan.to_vec())
 }
+
+
+
+pub async fn get_known_wifi_list() -> Result<Vec<NetworkResult>> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    info!("Starting wifi-sta example");
+
+    let mut setup = sta::WifiSetup::new()?;
+
+    let proposed_path = format!("/var/run/wpa_supplicant/wlan0");
+    setup.set_socket_path(proposed_path);
+
+    let broadcast = setup.get_broadcast_receiver();
+    let requester = setup.get_request_client();
+    let runtime = setup.complete();
+
+    let (_runtime, known_wifi, _broadcast) = tokio::join!(
+        async move {
+            if let Err(e) = runtime.run().await {
+                error!("Error: {}", e);
+            }
+        },
+        known_wifi(requester),
+        broadcast_listener(broadcast),
+    );
+
+    let wifi_list = known_wifi.unwrap();
+    Ok(wifi_list)
+}
+
+async fn known_wifi(requester: sta::RequestClient) -> Result<Vec<NetworkResult>> {
+    info!("Requesting scan");
+    let scan = requester.get_networks().await?;
+    info!("Scan complete");
+    info!("Shutting down");
+    requester.shutdown().await?;
+    Ok(scan)
+}
+
 
 async fn broadcast_listener(mut broadcast_receiver: sta::BroadcastReceiver) -> Result {
     while let Ok(broadcast) = broadcast_receiver.recv().await {
