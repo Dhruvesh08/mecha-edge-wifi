69c2faa29f664e12e1486b3e0eed545d1561ed9f
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-08-24 17:24:47 +0530

    wifi moduled added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
-// main.rs (in your project)
+// // main.rs (in your project)
+// mod wifi_lib;
+// use wifi_lib::get_wifi_list;
+
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     let wifi_list = get_wifi_list().await?;
+//     println!("Wi-Fi networks: {:?}", wifi_list);
+//     Ok(())
+// }
+
+//defaul rust hello world program
+
+use log::info;
+use wifi_lib::{Broadcast, WifiManager};
+
 mod wifi_lib;
-use wifi_lib::get_wifi_list;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let wifi_list = get_wifi_list().await?;
-    println!("Wi-Fi networks: {:?}", wifi_list);
-    Ok(())
+async fn main() {
+    let mut wifi_manager = WifiManager::new("wlan0").unwrap();
+    // Scan for wifi networks and print the results
+    let scan_results = wifi_manager.scan_wifi().await.unwrap();
+    for scan_result in scan_results.iter() {
+        info!("   {:?}", scan_result);
+    }
+
+    loop {
+        let broadcast = wifi_manager.listen_broadcast().await.unwrap();
+        match broadcast {
+            Broadcast::Connected => info!("Connected to wifi"),
+            Broadcast::Disconnected => info!("Disconnected from wifi"),
+            Broadcast::NetworkNotFound => todo!(),
+            Broadcast::WrongPsk => todo!(),
+            Broadcast::Ready => todo!(),
+        }
+    }
 }
--- src/wifi_lib.rs
+++ src/wifi_lib.rs
@@ -2,47 +2,68 @@
 
 use env_logger::Env;
 use log::{error, info};
-use wifi_ctrl::{sta::{self, ScanResult}, Result};
+use wifi_ctrl::{sta, Result};
 
-pub async fn get_wifi_list() -> Result<Vec<ScanResult>> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    info!("Starting wifi-sta example");
+pub use sta::{Broadcast, KeyMgmt, NetworkResult, ScanResult, SelectResult};
 
-    let mut setup = sta::WifiSetup::new()?;
+// Define a struct that represents a wifi manager
+pub struct WifiManager {
+    requester: sta::RequestClient,
+    broadcast_receiver: sta::BroadcastReceiver,
+}
 
-    let proposed_path = format!("/var/run/wpa_supplicant/wlan0");
-    setup.set_socket_path(proposed_path);
-
-    let broadcast = setup.get_broadcast_receiver();
-    let requester = setup.get_request_client();
-    let runtime = setup.complete();
-
-    let (_runtime, app, _broadcast) = tokio::join!(
-        async move {
+impl WifiManager {
+    // Implement a constructor that takes a network interface name as an argument
+    pub fn new(interface_name: &str) -> Result<Self> {
+        let mut setup = sta::WifiSetup::new()?;
+        let proposed_path = format!("/var/run/wpa_supplicant/{}", interface_name);
+        info!("Setting socket path to {}", proposed_path);
+        setup.set_socket_path(proposed_path);
+        let broadcast_receiver = setup.get_broadcast_receiver();
+        let requester = setup.get_request_client();
+        let runtime = setup.complete();
+        // Spawn a task that runs the runtime in the background
+        tokio::spawn(async move {
             if let Err(e) = runtime.run().await {
-                error!("Error: {}", e);
+                error!("Error: {e}");
             }
-        },
-        app(requester),
-        broadcast_listener(broadcast),
-    );
-
-    let wifi_list = app.unwrap();
-    Ok(wifi_list)
-}
-
-async fn app(requester: sta::RequestClient) -> Result<Vec<ScanResult>> {
-    info!("Requesting scan");
-    let scan = requester.get_scan().await?;
-    info!("Scan complete");
-    info!("Shutting down");
-    requester.shutdown().await?;
-    Ok(scan.to_vec())
-}
-
-async fn broadcast_listener(mut broadcast_receiver: sta::BroadcastReceiver) -> Result {
-    while let Ok(broadcast) = broadcast_receiver.recv().await {
-        info!("Broadcast: {:?}", broadcast);
+        });
+        Ok(Self {
+            requester,
+            broadcast_receiver,
+        })
     }
-    Ok(())
+
+    // Implement a method that scans for wifi networks
+    pub async fn scan_wifi(&self) -> Result<Vec<ScanResult>> {
+        info!("Requesting scan");
+        let scan = self.requester.get_scan().await?;
+        info!("Scan complete");
+        Ok(scan.to_vec())
+    }
+
+    // Implement a method that connects to a wifi network by id
+    pub async fn connect_wifi(&self, network_id: usize) -> Result<SelectResult> {
+        info!("Selecting network {}", network_id);
+        let result = self.requester.select_network(network_id).await?;
+        info!("Select result: {}", result);
+        Ok(result)
+    }
+
+    // Implement a method that forgets a wifi network by id
+    pub async fn forget_wifi(&self, network_id: usize) -> Result {
+        info!("Removing network {}", network_id);
+        self.requester.remove_network(network_id).await?;
+        info!("Network removed");
+        Ok(())
+    }
+
+    // Implement a method that listens for broadcast events
+    pub async fn listen_broadcast(&mut self) -> Result<Broadcast> {
+        let broadcast = self.broadcast_receiver.recv().await.unwrap();
+        info!("Broadcast: {:?}", broadcast);
+        Ok(broadcast)
+    }
 }
+
+
